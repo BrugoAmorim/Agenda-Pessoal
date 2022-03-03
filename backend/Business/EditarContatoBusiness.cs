7377865628020a2f76ca257cf92c90ce2df5d295
BrugoAmorim <brunogomes.concomercial@gmail.com>
2022-03-03 10:11:49 -0300

    arrumando regras de negocio

--- backend/Business/EditarContatoBusiness.cs
+++ backend/Business/EditarContatoBusiness.cs
@@ -18,12 +18,6 @@ namespace backend.Business
             if(string.IsNullOrEmpty(req.nome))
                 throw new ArgumentException("É necessário preencher o nome do contato");
 
-            if(req.celular.Length != 12 && req.celular.Length != 13 && req.celular != "")
-                throw new ArgumentException("Número de celular inválido");
-
-            if(req.telefone.Length != 8 && req.telefone.Length != 9 && req.telefone != "")
-                throw new ArgumentException("Número de telefone inválido");
-
             Models.TbContato obj = banco.atualizarinfo(req, id);
             return obj;
         }
