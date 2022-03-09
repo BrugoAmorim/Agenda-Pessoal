adaa074e0c560e14eb49ab53f2928d99eab7ae81
BrugoAmorim <brunogomes.concomercial@gmail.com>
2022-03-09 11:44:10 -0300

    subindo parte 2 do projeto

--- Analise-requisitos/Banco de Dados/script-banco.sql
+++ Analise-requisitos/Banco de Dados/script-banco.sql
@@ -13,8 +13,18 @@ ds_estado varchar(50),
 ds_notas varchar(80)
 );
 
+create table tb_bloco_nota(
+id_bloco_nota int primary key auto_increment,
+nm_bloco varchar(50),
+ds_conteudo varchar(500),
+dt_criacao datetime,
+dt_atualizado datetime
+);
+
+select * from tb_bloco_nota;
 select * from tb_contato;
 
+/* inserções na tabela contato */
 insert into tb_contato(nm_contato, ds_celular, ds_telefone, ds_email, ds_cidade, ds_estado, ds_notas)
 values('Kaio Junior', '11 9999-9999', '1111-2222', 'kkaio@gmail.com', 'sao paulo', 'sao paulo', 'amigo do trabalho');
 
@@ -32,3 +42,10 @@ values('Felipe Santos', '11 9999-9999', '1111-2222', 'felps2020@gmail.com', 'Rec
 
 insert into tb_contato(nm_contato, ds_celular, ds_telefone, ds_cidade, ds_estado, ds_notas)
 values('Victor Hugo', '11 9999-9999', '1111-2222', 'sao paulo', 'sao paulo', 'sobrinho');
+
+/* inserções na tabela bloco de notas */
+insert into tb_bloco_nota(nm_bloco, ds_conteudo, dt_criacao, dt_atualizado)
+values('lista de compras', 'comprar carne e temperos', '2021-02-10', '2021-04-12');
+
+insert into tb_bloco_nota(nm_bloco, ds_conteudo, dt_criacao, dt_atualizado)
+values('trabalhos para fazer', 'trabalho de geo, trabalho de hist e trabalho de pt', '2022-02-08', '2022-02-12');
