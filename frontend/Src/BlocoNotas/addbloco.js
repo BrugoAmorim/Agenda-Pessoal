ece317c00c68131d92e1f81fd29efc027ceef654
BrugoAmorim <brunogomes.concomercial@gmail.com>
2022-03-16 09:56:16 -0300

    subindo js tela nova-anotacao

---
+++ frontend/Src/BlocoNotas/addbloco.js
@@ -0,0 +1,36 @@
+
+
+let texto = document.getElementById("inpConteudo");
+let titulo = document.getElementById("inpTitulo");
+
+let salvar = document.getElementById("btnSalvar");
+
+salvar.onclick = async () => {
+
+    let obj = {
+        nome: titulo.value,
+        texto: texto.value
+    };
+
+    let url = "http://localhost:5000/BlocoNotas/adicionar-bloco";
+
+    const api = await fetch(url,{
+
+        mode:'cors',
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+
+        body: JSON.stringify(obj)
+    });
+
+    let caixote = api.json();
+    caixote.then(caixote => {
+
+        if(caixote.codigo == 400)
+            swal("Algo deu errado!", caixote.mensagem, "error");
+        else
+            swal("Texto Criado!", "\"" + caixote.nome + "\"" + " Já foi Salvo!", "success");
+    })
+}
