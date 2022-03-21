143dae7b6cd86e0bb6db331ea1a9de1867f77f95
BrugoAmorim <brunogomes.concomercial@gmail.com>
2022-03-20 22:30:48 -0300

    add funcao excluir-anotacao

--- frontend/Src/BlocoNotas/infobloco.js
+++ frontend/Src/BlocoNotas/infobloco.js
@@ -6,6 +6,7 @@ let txtBloco = document.getElementById("txtConteudo");
 let atualizado = document.getElementById("dtAtualizado");
 let criado = document.getElementById("dtCriado");
 
+// modelo que contem as informacoes do arquivo
 let obj = BuscarInfoTemporarias();    
 
 // metodo que cria um modelo da data de criacao
@@ -55,6 +56,55 @@ btnAtualizar.onclick = async () => {
         else
             swal("Alterado com sucesso!", "O seu registro já foi atualizado.", "success");
     });
+}
+
+let btnExcluir = document.getElementById("Excluir");
+
+btnExcluir.onclick = () => {
+    
+    let id = obj.idbloco;
+
+    swal("Aviso!", "Você tem certeza que deseja excluir o arquivo?", "warning",{
+        buttons: {
+            cancel: "Não",
+            catch:{
+                text: "Sim",
+                value: "acionarevento",
+            },
+        },
+    }).then((value) => {
+        switch(value){
+
+        case "acionarevento":
+            // se o usuario clicar em 'sim', a funcao de excluir será chamada e apagará o arquivo
+            Excluir(id);
+            break;
+
+        default:
+            break;
+        }
+    });
+}
+
+// metodo que exclui os arquivos do sistema
+async function Excluir(idanotacao){
+
+    let url = "http://localhost:5000/BlocoNotas/excluir-bloco/" + idanotacao;
+
+    const API = await fetch(url, {
+
+        method:'DELETE',
+        mode:'cors'
+    });
+
+    let objeto = API.json();
+    objeto.then(objeto => {
+
+        if(objeto.codigo == 400)
+            swal("Algo deu Errado!", objeto.mensagem, "error");
+        else
+            swal(objeto.mensagem, "", "success");
+    })
 }
 
 let btnVoltar = document.getElementById("Voltar");
