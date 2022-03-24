fcafa37ccef348dae0e23bb48ae01bcd64bd5b9e
BrugoAmorim <brunogomes.concomercial@gmail.com>
2022-03-24 11:27:46 -0300

    subindo telas-lista-tarefas e atualizando script bd

--- Analise-requisitos/Banco de Dados/script-banco.sql
+++ Analise-requisitos/Banco de Dados/script-banco.sql
@@ -21,8 +21,51 @@ dt_criacao datetime not null,
 dt_atualizado datetime not null
 );
 
+create table tb_categoria(
+id_categoria int primary key auto_increment,
+nm_categoria varchar(100) not null,
+ds_categoria varchar(300) not null
+);
+
+create table tb_tarefa(
+id_tarefa int primary key auto_increment,
+ds_tarefa varchar(200) not null,
+bl_concluida bool not null,
+vl_prioridade int not null,
+dt_adicionado datetime not null,
+id_categoria int not null,
+
+foreign key (id_categoria) references tb_categoria(id_categoria)
+);
+
+select * from tb_categoria;
+select * from tb_tarefa;
 select * from tb_bloco_nota;
 select * from tb_contato;
+
+/* inserções na tabela categoria */
+insert into tb_categoria(nm_categoria, ds_categoria)
+values("Estudos", "Algumas coisas que seria bom eu estudar, conteudo que preciso rever para a prova e trabalhos escolares");
+
+insert into tb_categoria(nm_categoria, ds_categoria)
+values("Mercado", "Lista das compras que farei no mercado este ano.");
+
+/* inserções na tabela tarefa */
+
+/* inserindo registros na tabela tarefa, na categoria Estudos */
+insert into tb_tarefa(ds_tarefa, bl_concluida, vl_prioridade, dt_adicionado, id_categoria)
+values('Estuda NodeJs', false, 2, '2022-03-10', 1);
+
+insert into tb_tarefa(ds_tarefa, bl_concluida, vl_prioridade, dt_adicionado, id_categoria)
+values('Rever a Materia de ingles', false, 1, '2022-02-01', 1);
+
+/* inserindo registros na tabela tarefa, na categoria Mercado */
+insert into tb_tarefa(ds_tarefa, bl_concluida, vl_prioridade, dt_adicionado, id_categoria)
+values('Compras do Mes: Arroz, feijao, carne, temperos, e verduras', true, 1, '2022-02-01', 2);
+
+insert into tb_tarefa(ds_tarefa, bl_concluida, vl_prioridade, dt_adicionado, id_categoria)
+values('Ingredientes Receita do Bolo: ovos, leite, fermento e farinha', false, 4, '2022-01-09', 2);
+
 
 /* inserções na tabela contato */
 insert into tb_contato(nm_contato, ds_celular, ds_telefone, ds_email, ds_cidade, ds_estado, ds_notas)
@@ -42,6 +85,7 @@ values('Felipe Santos', '11 9999-9999', '1111-2222', 'felps2020@gmail.com', 'Rec
 
 insert into tb_contato(nm_contato, ds_celular, ds_telefone, ds_cidade, ds_estado, ds_notas)
 values('Victor Hugo', '11 9999-9999', '1111-2222', 'sao paulo', 'sao paulo', 'sobrinho');
+
 
 /* inserções na tabela bloco de notas */
 insert into tb_bloco_nota(nm_bloco, ds_conteudo, dt_criacao, dt_atualizado)
